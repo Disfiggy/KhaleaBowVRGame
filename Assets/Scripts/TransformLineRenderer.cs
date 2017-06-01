7af56d1517cd4149789935745a8d1f9bc794bded
Kimmo <kimmo.kaisla@gmail.com>
2017-06-01 14:52:47 +0300

    Added string to the bow. Improved the arrow.

---
+++ Assets/Scripts/TransformLineRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformLineRenderer : MonoBehaviour
+{
+    [SerializeField]
+    private LineRenderer _lineRendererRef;
+
+    [SerializeField]
+    private Transform[] _transforms;
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        for (int i = 0; i < _transforms.Length; i++)
+        {
+            _lineRendererRef.SetPosition(i, _transforms[i].position);
+        }
+	}
+}
